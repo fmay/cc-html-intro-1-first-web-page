84cacabbc1a54b73329d4fec3895b8799ccdf02a
fmay <fmay@applicationcraft.com>
2014-08-18 13:36:43 +0000

    corrected restore

--- .guides/restore.sh
+++ .guides/restore.sh
@@ -5,15 +5,23 @@ red='\e[0;31m'
 green='\e[0;32m'
 NC='\e[0m' # No Color
 
+echo
+echo '-------------------------------------'
 echo -e "${red}Restoring your workspace${NC}"
 
 # Temporary until Codio Guides is done. Copy annotations file to safety
-cp ~/workspace/annotations.md ~/workspace/.guides
+cp -r ~/workspace/annotations.md ~/workspace/.guides/annotations.md
 
 # Remove the entire contents of the root folder
 rm -rf ~/workspace/* 
 
 # Copy the example folder to the root
-cp -r ~/workspace/.guides/arg[0]/* ~/workspace
+cp -r ~/workspace/.guides/$1/* ~/workspace
+
+# Temporary until Codio Guides is done. Restore annotations.md file
+cp -r ~/workspace/.guides/annotations.md ~/workspace
+
 
 echo -e "${red}Done!!! You can close the tab now.${NC}"
+echo '-------------------------------------'
+echo
