837fa38f2ea41415a809819639de4025028fbd04
ianjobling <ijobling@codio.com>
2014-09-01 15:31:47 +0000

    update guides

--- .guides/restore.sh
+++ .guides/restore.sh
@@ -9,18 +9,11 @@ echo
 echo '-------------------------------------'
 echo -e "${red}Restoring your workspace${NC}"
 
-# Temporary until Codio Guides is done. Copy annotations file to safety
-cp -r ~/workspace/annotations.md ~/workspace/.guides/annotations.md
-
 # Remove the entire contents of the root folder
 rm -rf ~/workspace/* 
 
 # Copy the example folder to the root
 cp -r ~/workspace/.guides/$1/* ~/workspace
-
-# Temporary until Codio Guides is done. Restore annotations.md file
-cp -r ~/workspace/.guides/annotations.md ~/workspace
-
 
 echo -e "${red}Done!!! You can close the tab now.${NC}"
 echo '-------------------------------------'
